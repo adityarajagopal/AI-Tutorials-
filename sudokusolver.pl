b5cf2f6eb46d4fde86db765a18c7df60c52d8bf8
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-11-07 21:05:19 +0000

    sudokusolver

--- sudokusolver.pl
+++ sudokusolver.pl
@@ -1,12 +1,20 @@
 %
+/*
 check_repeat([]):-!.
 check_repeat([H|List]):-
 	\+member(H,List),
 	check_repeat(List).
+*/
+
+check_repeat([],[]):-!.
+check_repeat([H|List],StandardList):-
+	member(H,StandardList),
+	delete(StandardList,H,NewList),
+	check_repeat(List,NewList).
 
 check_rows([]):-!.
 check_rows([H1,H2,H3,H4,H5,H6,H7,H8,H9|List]):-
-	check_repeat([H1,H2,H3,H4,H5,H6,H7,H8,H9]),
+	check_repeat([H1,H2,H3,H4,H5,H6,H7,H8,H9],[1,2,3,4,5,6,7,8,9]),
 	check_rows(List).
 
 transpose([],[],[],[],[],[],[],[],[],[]):-!.
@@ -22,19 +30,7 @@ extract_box(List,Box1,Box2,Box3):-
 	transpose(List,AT1,AT2,AT3),
 	append(AT1,AT2,Temp),
 	append(Temp,AT3,List1),
-	get_3_columns(List1,Box1T,Box2T,Box3T),
-	%Get Box1
-	transpose(Box1T,Box11,Box12,Box13),
-	append(Box11,Box12,Temp1),
-	append(Temp1,Box13,Box1),
-	%Get Box2
-	transpose(Box2T,Box21,Box22,Box23),
-	append(Box21,Box22,Temp2),
-	append(Temp2,Box23,Box2),
-	%Get Box3
-	transpose(Box3T,Box31,Box32,Box33),
-	append(Box31,Box32,Temp3),
-	append(Temp3,Box33,Box3).
+	get_3_columns(List1,Box1,Box2,Box3).
 
 check_cols(List):-
 	transpose(List,A,B,C,D,E,F,G,H,I),
@@ -63,9 +59,14 @@ check_box(List):-
 	check_rows(Box2C),
 	check_rows(Box3C).
 
-run():-
-	check_box([1,8,3,2,4,5,6,7,9,2,4,5,6,7,9,1,3,8,6,7,9,1,3,8,2,4,5,3,1,2,4,5,6,8,9,7,4,5,6,8,9,7,3,1,2,7,9,8,3,1,2,4,5,6,5,2,1,7,6,3,9,8,4,8,3,7,9,2,4,5,6,1,9,6,4,5,8,1,7,2,3]).
-	%check_box([1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9]).
+check_sudoku(List):-
+	check_rows(List),
+	check_cols(List),
+	check_box(List).
+
+run(A2):-
+	%check_sudoku([1,8,3,2,4,5,6,7,9,A,4,5,6,7,9,1,3,8,6,7,9,1,3,8,2,4,5,3,1,2,4,5,6,8,9,7,4,5,6,8,9,7,3,1,2,7,9,8,3,1,2,4,5,6,5,2,1,7,6,3,9,8,4,8,3,7,9,2,4,5,6,1,9,6,4,5,8,1,7,2,3]).
+	check_sudoku([8,A2,A3,A4,A5,A6,A7,A8,A9,B1,B2,3,6,B5,B6,B7,B8,B9,C1,7,C3,C4,9,C6,2,C8,C9,D1,5,D3,D4,D5,7,D7,D8,D9,E1,E2,E3,E4,4,5,7,E8,E9,F1,F2,F3,1,F5,F6,F7,3,F9,G1,G2,1,G4,G5,G6,G7,6,8,H1,H2,8,5,H5,H6,H7,1,H9,I1,9,I3,I4,I5,I6,4,I8,I9]).
 	
 	
 
